c39e6c1ee9b39a3af5f4e7d5720d20b71cbce619
Lester Andres Garcia Aquino <lagarcia2015url@gmail.com>
2022-11-05 16:23:09 -0600

    Arreglando solución al problema 01

--- Solución - Nivel 02/Problema 01/Problema01/src/main/java/org/hackathon/Main.java
+++ Solución - Nivel 02/Problema 01/Problema01/src/main/java/org/hackathon/Main.java
@@ -6,13 +6,31 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        ArrayList<String> lista1 = new ArrayList<>();
+        ArrayList<String> lista2 = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce una cadena");
         String cadena01 = sc.nextLine();
         System.out.println("Introduce otra cadena");
         String cadena02 = sc.nextLine();
 
-        if(cadena01.contains(cadena02) && cadena02.contains(cadena01)){
+        for (int i = 0; i < cadena01.length(); i++) {
+            for (int j = i + 1; j <= cadena01.length(); j++) {
+                String r = cadena01.substring(i, j);
+                lista1.add(r);
+            }
+        }
+        for (int i = 0; i < cadena02.length(); i++) {
+            for (int j = i + 1; j <= cadena02.length(); j++) {
+                String r = cadena02.substring(i, j);
+                lista2.add(r);
+            }
+        }
+
+        Collections.sort(lista1);
+        Collections.sort(lista2);
+
+        if(lista1 == lista2){
             System.out.println("TRUE");
         }else{
             System.out.println("FALSE");
